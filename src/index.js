74cd30b398ff0fa9195a21a31e50806607756234
evgenych@gmail.com <evgenych@gmail.com>
2020-10-01 00:31:21 +0300

    fix: function min and max

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
 exports.min = function min (array) {
+    if ( array == 0 || array == '' || array == undefined )
+        return 0;
+var result = array[0];
+        for ( var i = 1; i < array.length; i++) {
+            if (result > array[i])
+            result = array[i];
+        }
+    return result;
+}
+exports.max = function max (array) {
+    if ( array == 0 || array == '' || array == undefined)
   return 0;
+  var result = array[0];
+  for ( var i = 1; i < array.length; i++) {
+      if (result < array[i])
+      result = array[i];
+  }
+return result;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
 
 exports.avg = function avg (array) {
+    if ( array == 0 || array == '' || array == undefined)
   return 0;
 }
