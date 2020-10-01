7efbf6a4beb4442816743c9721930f5ea92525c3
evgenych@gmail.com <evgenych@gmail.com>
2020-10-01 11:55:32 +0300

    implement: function avg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 exports.min = function min (array) {
     if ( array == 0 || array == '' || array == undefined )
         return 0;
-var result = array[0];
-        for ( var i = 1; i < array.length; i++) {
+let result = array[0];
+        for ( let i = 1; i < array.length; i++) {
             if (result > array[i])
             result = array[i];
         }
@@ -11,8 +11,8 @@ var result = array[0];
 exports.max = function max (array) {
     if ( array == 0 || array == '' || array == undefined)
   return 0;
-  var result = array[0];
-  for ( var i = 1; i < array.length; i++) {
+let result = array[0];
+  for ( let i = 1; i < array.length; i++) {
       if (result < array[i])
       result = array[i];
   }
@@ -23,4 +23,14 @@ return result;
 exports.avg = function avg (array) {
     if ( array == 0 || array == '' || array == undefined)
   return 0;
+
+let wholeAmount = array[0];
+let result = 0;
+
+      for ( let i = 1; i < array.length; i++) {
+          wholeAmount += array[i];
+      }
+  result = wholeAmount / array.length;
+    
+  return result;
 }
